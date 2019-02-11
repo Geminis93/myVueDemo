b03cc1b7e6475c9647c4d0976336c8798c6eccea
17600596130 <A932635@163.com>
2019-02-11 17:21:48 +0800

    feat( page1 ):todo

---
+++ src/views/Page1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="onChange()">aaa</button>
+    <ul>
+      <li v-for="number in numberList" :key="number">{{ number }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "page1",
+  data() {
+    return {
+      numberList: [1, 2, 3, 4, 5]
+    };
+  },
+  methods: {
+    onChange() {
+      this.numberList = ["a", "b", "c", "d", "e"];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>

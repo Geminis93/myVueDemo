5af473baf67b79fcb13f79206768553a91e3a881
17600596130 <A932635@163.com>
2019-02-11 19:02:32 +0800

    feat( add ):canvas-nest

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <button @click="onChange()">aaa</button>
-    <ul>
-      <li v-for="number in numberList" :key="number">{{ number }}</li>
-    </ul>
+  <div class="page">
+    <div class="page-content">
+      <router-link to="/">aa</router-link> | 
+      <button @click="onChange()">aaa</button>
+      <ul>
+        <li v-for="number in numberList" :key="number">{{ number }}</li>
+      </ul>
+    </div>
+    <div ref="canvasNest" class="page-bg" />
   </div>
 </template>
 
 <script>
+import CanvasNest from 'canvas-nest.js';
+
 export default {
   name: "page1",
   data() {
     return {
+      canvasNest: null,
       numberList: [1, 2, 3, 4, 5]
     };
+  },
+  mounted() {
+    this.canvasNest = new CanvasNest(this.$refs.canvasNest);
+  },
+  destroyed() {
+    this.canvasNest.destroy();
+    this.canvasNest = null;
   },
   methods: {
     onChange() {
@@ -23,4 +37,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './Page1.scss';
+</style>
